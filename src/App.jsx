55c1a606dfaeced82971da32333c85818757ba61
JENTAH REHEMA <jentahrehema7@gmail.com>
2025-04-15 11:24:51 +0300

    Setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-gray-100 min-h-screen rounded-2xl ">
+        <h1 className="text-2xl  text-center font-extrabold">Goal Planner</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
