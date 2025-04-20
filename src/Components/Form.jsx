ab552a4b227add4b9dcfe331b258e41ee18b7f0d
JENTAH REHEMA <jentahrehema7@gmail.com>
2025-04-20 20:55:17 +0300

    Updating goal planner app

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Goals from "./Goals";
 
 const Form = () => {
+  const [editedGoal, setEditedGoal] = useState(null);
+  const [goals, setGoals] = useState(() => {
+    const storedGoals = localStorage.getItem("goals");
+    return storedGoals ? JSON.parse(storedGoals) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("goals", JSON.stringify(goals));
+  }, [goals]);
+
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -17,7 +27,22 @@ const Form = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
+
+    if (editedGoal !== null) {
+      const updated = goals.map((goal) => {
+        if (goal.id === editedGoal) {
+          return { ...goal, ...formData };
+        }
+        return goal;
+      });
+
+      setGoals(updated);
+      setEditedGoal(null);
+    } else {
+      const newGoal = { ...formData, id: Date.now() };
+
+      setGoals((prevgoals) => [...prevgoals, newGoal]);
+    }
 
     setFormData({
       title: "",
@@ -27,53 +52,59 @@ const Form = () => {
       status: "not-started",
     });
   };
+
+  const handleDelete = (id) => {
+    const updated = goals.filter((goal) => goal.id !== id);
+
+    setGoals(updated);
+    setEditedGoal(null);
+  };
+
+  const handleEdit = (id) => {
+    const updated = goals.find((goal) => goal.id === id);
+    const { title, description, category, deadline, status } = updated;
+    if (updated) {
+      setFormData({ title, description, category, deadline, status });
+      setEditedGoal(id);
+    }
+  };
+
+  const markAsDone = (id) => {
+    const updated = goals.map((goal) =>
+      goal.id === id ? { ...goal, status: "done" } : goal
+    );
+
+    setGoals(updated);
+  };
+
   return (
     <div>
-      <div className=" w-64 md:w-72 lg:w-96  border border-gray-300 rounded-3xl mx-auto shadow shadow-gray-500 ">
-        <h1 className="font-bold text-2xl  m-10 ">Add Goal</h1>
+      <div className=" w-1/2 max-w-4xl  border border-gray-300 rounded-3xl mx-auto shadow shadow-gray-500 ">
+        <h1 className="font-bold text-2xl  text-center "> 🎯Add New Goal</h1>
         <div className="flex justify-center items-center m-10   ">
           <form onSubmit={handleSubmit}>
-            <label for="title" className="text-base font-semibold block mt-3">
-              Title
-            </label>
-            <input
-              type="text"
-              name="title"
-              value={formData.title}
-              onChange={handleFormInput}
-              className="focus:outline-none border border-gray-400 rounded w-full  "
-            />
-            <label
-              for="description"
-              className="text-base font-semibold block mt-3"
-            >
-              Description
-            </label>
-            <input
-              type="text"
-              name="description"
-              value={formData.description}
-              onChange={handleFormInput}
-              className="focus:outline-none border border-gray-400 rounded w-full   "
-            />
-            <div className="flex gap-4 mt-3">
-              <div className="w-1/2">
+            {["title", "description", "category"].map((field) => (
+              <div key={field}>
                 <label
-                  for="category"
-                  className="text-base font-semibold  block"
+                  htmlFor={field}
+                  className="text-md font-semibold capitalize block mt-3"
                 >
-                  Category
+                  {field}
                 </label>
                 <input
                   type="text"
-                  name="category"
-                  value={formData.category}
+                  id={field}
+                  name={field}
+                  value={formData[field]}
                   onChange={handleFormInput}
-                  className="focus:outline-none border border-gray-400 rounded  w-full  "
+                  className="w-full border rounded focus:outline-blue-400 px-3 py-2 "
                 />
               </div>
-              <div className="w-1/2">
-                <label for="deadline" className="text-base font-semibold ">
+            ))}
+
+            <div className="grid grid-cols-2 gap-3 mt-3">
+              <div>
+                <label htmlFor="deadline" className="text-base font-semibold ">
                   Deadline
                 </label>
                 <input
@@ -81,36 +112,43 @@ const Form = () => {
                   name="deadline"
                   value={formData.deadline}
                   onChange={handleFormInput}
-                  className="focus:outline-none border border-gray-400 rounded w-full"
+                  className="focus:outline-none border border-gray-400 rounded w-full px-3 py-2"
                 />
               </div>
+              <div>
+                <label htmlFor="status" className="block  font-semibold">
+                  Status
+                </label>
+                <select
+                  name="status"
+                  id="status"
+                  value={formData.status}
+                  onChange={handleFormInput}
+                  className="w-full border border-gray-300 rounded focus:outline-none px-3 py-2"
+                >
+                  <option value="not-started">Not-Started</option>
+                  <option value="in-progress">In-Progress</option>
+                  <option value="done">Done</option>
+                </select>
+              </div>
             </div>
-            <label for="status" className="block mt-3 font-semibold">
-              Status
-            </label>
-            <select
-              name="status"
-              id="status"
-              value={formData.status}
-              onChange={handleFormInput}
-              className="w-full border border-gray-300 rounded focus:outline-none"
-            >
-              <option value="not-started">Not-Started</option>
-              <option value="in-progress">In-Progress</option>
-              <option value="done">Done</option>
-            </select>
             <div className="mt-3 flex items-end justify-end">
               <button
                 type="submit"
-                className="bg-blue-500 border-none rounded text-base p-2"
+                className="bg-blue-600 border-none rounded text-base p-2 text-white hover:bg-blue-400 transition"
               >
-                Add Goal
+                {editedGoal !== null ? "Edited Goal" : "Add Goal"}
               </button>
             </div>
           </form>
         </div>
       </div>
-      <Goals formData={formData} />
+      <Goals
+        goals={goals}
+        handleDelete={handleDelete}
+        handleEdit={handleEdit}
+        handleMarkAsDone={markAsDone}
+      />
     </div>
   );
 };
--- src/Components/Goals.jsx
+++ src/Components/Goals.jsx
@@ -1,26 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Goals = () => {
+const Goals = ({ goals, handleDelete, handleEdit, handleMarkAsDone }) => {
+  const [filteredstatus, setFilteredStatus] = useState("all");
+  const [sortedArr, setSortedArr] = useState("newest");
+
+  const filteredGoals =
+    filteredstatus === "all"
+      ? goals
+      : goals.filter((goal) => goal.status === filteredstatus);
+
+  const sortedGoals = [...filteredGoals].sort((a, b) => {
+    if (sortedArr === "newest") {
+      return new Date(b.deadline) - new Date(a.deadline);
+    } else {
+      return new Date(a.deadline) - new Date(b.deadline);
+    }
+  });
+
+  const colorStatus = {
+    "not-started": "bg-red-200 text-red-800",
+    "in-progress": "bg-yellow-200 text-yellow-900",
+    done: "bg-green-200 text-green-800",
+  };
   return (
     <div>
       <div className="w-64 md:w-72 lg:w-96 mx-auto mt-5">
         <div className="flex justify-between">
-          <label for="filter">Filter By Status</label>
-          <input
-            type="text"
-            className="border border-gray-400 rounded focus:outline-none p-2 h-10"
-          />
+          <label for="filter" className="text-lg font-semibold">
+            Filter By Status
+          </label>
+          <select
+            name="filter"
+            value={filteredstatus}
+            onChange={(event) => setFilteredStatus(event.target.value)}
+            id="filter"
+            className="border border-gray-400 rounded focus:outline-none h-10"
+          >
+            <option value="all">All</option>
+            <option value="not-started">Not Started</option>
+            <option value="in-progress">In progress</option>
+            <option value="done">Done</option>
+          </select>
 
-          <label for="sort">Sort By</label>
+          <label for="sort" className="text-lg font-semibold">
+            Sort By
+          </label>
           <select
             name="sort"
             id="sort"
+            value={sortedArr}
+            onChange={(e) => setSortedArr(e.target.value)}
             className="border border-gray-400 rounded focus:outline-none h-10"
           >
             <option value="newest">Newest</option>
             <option value="oldest">Oldest</option>
           </select>
         </div>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 w-1/2 mx-auto mt-2">
+        {sortedGoals.length === 0 ? (
+          <p className="text-gray-800 text-2xl">No goals added yet!</p>
+        ) : (
+          sortedGoals.map((goal) => (
+            <div
+              key={goal.id}
+              className="mb-2 p-2 border border-blue-300 rounded bg-white mx-auto w-50  shadow-lg hover:shadow-blue-400 transition-transform transform hover:scale-125 duration-200 "
+            >
+              <div>
+                <h2 className="font-bold">Title:{goal.title}</h2>
+                <p
+                  className={`text-md/8 text-gray-700 font-semibold ${
+                    colorStatus[goal.status]
+                  }`}
+                >
+                  Status:{goal.status}
+                </p>
+                <p className="text-md/8 text-gray-700 font-semibold">
+                  Deadline:{goal.deadline}
+                </p>
+                <p className="text-md/8 text-gray-600 font-semibold">
+                  Category:{goal.category}
+                </p>
+                <p className="text-md/8 text-gray-600 font-semibold">
+                  Description:{goal.description}
+                </p>
+              </div>
+              <div className="flex gap-2 mt-3">
+                <button
+                  className="border bg-blue-500 rounded px-2 py-1 text-white"
+                  onClick={() => handleDelete(goal.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="border bg-blue-500 rounded px-2 py-1 text-white"
+                  onClick={() => handleEdit(goal.id)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="border bg-blue-500 rounded px-2 py-1 text-white"
+                  onClick={() => handleMarkAsDone(goal.id)}
+                >
+                  Done
+                </button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
